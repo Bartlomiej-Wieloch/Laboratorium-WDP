cc85e47644ff1c3f3b524ccf3ed2b58d3815e79c
Ozzy <151674182+OzzyTheHuman@users.noreply.github.com>
2024-10-15 20:44:33 +0200

    Completed zad9, zad10

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -5,6 +5,29 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello WSEI");
+            Console.WriteLine("");
+            zad9();
+            zad10();
         }
+
+        static void zad9()
+        {
+            Console.Write("##");
+            Console.Write("##");
+            Console.WriteLine("##");
+            Console.Write("##");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.WriteLine("##");
+            Console.Write("##");
+            Console.Write("##");
+            Console.WriteLine("##");
+        }
+
+        static void zad10()
+        {
+            Console.WriteLine("\t123\n+\t235\n\t---\n\t358");
+        }
+
     }
 }
