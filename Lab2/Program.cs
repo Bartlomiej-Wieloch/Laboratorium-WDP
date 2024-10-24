beba8887892d12aaeb2e8c75e6bce485c1dc6a40
Bartlomiej_Wieloch <bartlomiej.wieloch@microsoft.wsei.edu.pl>
2024-10-24 22:08:11 +0200

    Create Lab3 files

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,119 +1,178 @@
-﻿namespace Lab2
+﻿using System.Threading.Channels;
+
+namespace Lab4;
+
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        //Arrays();
+        //Task1();
+        //ForInstruction();
+        //Task2();
+        //WhileInstruction();
+        //Task4();
+        ContinueInstruction();
+        //BreakInstruction();
+    }
+
+    public static void Arrays()
+    {
+        int[] numbers = { 2, 5, 1, 9, 8 };
+        double[] factors = new double[3]; //wypełniona dowolnymi wartościami dla double, czyli zerami
+        factors[2] = 5;
+        Console.WriteLine(numbers[0]);          //pierwszy element
+        Console.WriteLine(factors[^1]);         //pierwszy element od końca (od końca ma indeks 1 a nie 0)
+        Console.WriteLine(factors[^4]);         //gdy odwolamy sie do indeksu poza zakresem jest błąd, program przerywa działanie
+        Console.WriteLine(factors[3]);
+    }
+
+    // oblicz wartość funkcji kwadratowej dla z
+    // jeśli wpółczynniki równania zawiera tablica factors:
+    // pierwszy elem. to a 
+    // drugi to b
+    // trzeci to c
+    public static void Task1()
+    {
+        double[] factors = { 2.4, 2.6, 53 };
+        double x = 0.5;
+        double y = Math.Pow(x, 2) * factors[0] + x * factors[1] + factors[2];
+
+        Console.WriteLine(y);
+    }
+
+    public static void ForInstruction() //debugerem mozna przesledzic krok po kroku dzialanie pętli
+    {
+        char[] chars = { 'a', 'c', '%', '*', 'a' };
+        for (int i = 0; i < chars.Length; ++i)
         {
-             Zadanie1();
-             Zadanie2();
-             Zadanie3();
-             Zadanie4();
-             Zadanie5();
-             Zadanie6();
+            Console.WriteLine(chars[i]);
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+        int[] ints = { 2, 4, 1, 5, 7 };
+        //wyświetl komórki ints od pierwzej do ostaniej
+
+        for (int i = 0; i < ints.Length; ++i)
+        {
+            Console.WriteLine(ints[i]);
         }
 
-        /*
-         * oblicz wartości trzech zmiennych cyfraSetek, cyfraDziesiatek i cyfraJedności zakładając, 
-         * że w zmiennej number jest liczba mniejsza od 1000. 
-         */
-        public static void Zadanie1()
+        Console.WriteLine();
+        Console.WriteLine();
+        //od konca do początku
+        for (int i = 1; i < ints.Length + 1; i++) //--i/i-- ma znaczenie          
         {
-            Console.WriteLine("Zadanie 1\n");
-
-            int number = 837;
-            int cyfraSetek = number / 100;
-            //Console.WriteLine(cyfraSetek);
-            int cyfraDziesiątek = (number - (cyfraSetek * 100)) / 10;
-            //Console.WriteLine(cyfraDziesiątek);
-            int cyfraJedności = number - (cyfraSetek * 100) - (cyfraDziesiątek * 10);
-            //Console.WriteLine(cyfraJedności);
-
-            Console.WriteLine($"Cyfra setek: {cyfraSetek}\nCyfra dziesiątek: {cyfraDziesiątek}\nCyfra jedności: {cyfraJedności}");
+            Console.Write($"{ints[^i]} ");
         }
 
-        /*
-        * wstaw wyrażenie, który do zmiennej sinValue przypisze wartość funkcji sinus na podstawie kąta w stopniach ze zmiennej degree. Wartość zmiennej sinValue wyświetl w konsoli
-        */
-        public static void Zadanie2()
+        Console.WriteLine();
+
+        foreach (var number in ints)
         {
-            Console.WriteLine("\nZadanie 2\n");
-
-            double degree = 60;
-            double radians = degree * (Math.PI / 180);
-            double sinValue = Math.Sin(radians);
-
-            Console.WriteLine($"Wartość funkcji sinus: {sinValue}");
-        }
-
-        /*
-         * wpisz wyrażenie, które do zmiennej radius przypisze wartość promienia koła, którego pole powierzchni znajduje się w zmiennej area. Jeśli w zmiennej area jest liczba ujemna to promień powinien wynosić 0.
-         */
-        public static void Zadanie3()
-        {
-            Console.WriteLine("\nZadanie 3\n");
-
-            double area = -189;
-            if (area >= 0)
-            {
-                double radius = Math.Sqrt(area / Math.PI); //R=pierwiastek(Pole/PI)
-                Console.WriteLine($"Promień okręgu o polu {area}, wynosi {radius} ");
-            }
-            else
-            {
-                double radius = 0;
-                Console.WriteLine($"Promień okręgu o polu {area}, wynosi {radius} ");
-            }
-        }
-
-        /*
-         * wstaw wyrażenie przypisujące do zmiennej y wartość równania kwadratowego o współczynnikach w zmiennych a, b i c. Obliczoną zmienną wyświetl w konsoli. 
-         */
-        public static void Zadanie4()
-        {
-            Console.WriteLine("\nZadanie 4\n");
-
-            double x = 0.4;
-            double a = 1.5;
-            double b = 3.2;
-            double c = 4;
-            double y = a * Math.Pow(x, 2) + b * x + c;
-
-            Console.WriteLine(y);
-        }
-
-        /*
-         * Przypisz do zmiennej max, największa wartość spośród zmiennych a, b i c, a do zmiennej min najmniejszą. Wyświetl obie zmiennej wg wzoru:
-         * Max = 9
-         * Min = 2
-         */
-        public static void Zadanie5()
-        {
-            Console.WriteLine("\nZadanie 5\n");
-
-            int a = 9;
-            int b = 6;
-            int c = 2;
-            int max = Math.Max(a, Math.Max(b, c));
-            int min = Math.Min(a, Math.Min(b, c));
-
-            Console.WriteLine($"Max = {max}\nMin = {min}");
-
-        }
-        /*
-         * W zmiennej time znajduje się czas w minutach. Oblicz wartości zmiennych hours, minutes i seconds na postawie zmiennej time. Wyświetl obliczone wartości wg poniższego wzoru: <hours>:<minutes>:<seconds> 
-         */
-        public static void Zadanie6()
-        {
-            Console.WriteLine("\nZadanie 6\n");
-
-
-            double time = 1234.45; // czas w minutach
-            int hours = (int)time / 60;
-            int minutes = (int)time - hours * 60;
-            double fractinalSeconds = time - (int)time;
-            int seconds = (int)fractinalSeconds * 60;
-
-            Console.WriteLine($"{hours}:{minutes}:{seconds}");
+            Console.Write($"{number} ");
         }
     }
+
+    //Oblicz sumę elementów tablicy factors (za pomocą pętli)
+    public static void Task2()
+    {
+        double[] factors = { 3, 2.2, 53, 8.4, 4.6, 12 };
+        double sum = 0;
+        for (int i = 0; i < factors.Length; i++)
+        {
+            sum += factors[i];
+
+        }
+        Console.WriteLine(sum);
+    }
+
+    //Zmień liczby ujemne na ich wartość bezwględną w tablicy factors
+    public static void Task3()
+    {
+        double[] factors = { 3, 2.2, 53, 8.4, 4.6, 12 };
+        for (int i = 0; i < factors.Length; i++)
+        {
+            factors[i] = Math.Abs(factors[i]);
+        }
+    }
+
+    public static void WhileInstruction()
+    {
+        Console.WriteLine("co chcesz dalej zrobić: Q lub coś innego");
+        while (Console.ReadLine() != "Q") //gdy wpiszemy Q do konsoli, przerwie się pętla, jak coś innego, wykona się pętla
+        {
+            Console.WriteLine("Wykonję...");
+
+        }
+
+        Console.WriteLine("Koniec...");
+    }
+
+    //Napisz program, który wczytuje n liczb do tablicy
+    public static void Task4()
+    {
+        Console.WriteLine("Podaj ilość liczb:");
+        int n = int.Parse(Console.ReadLine());
+        int[] array = new int[n];
+        while (n-- > 0)
+        {
+            Console.WriteLine("Wpisz kolejną liczbę do tablicy: ");
+            array[^(n + 1)] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine(string.Join(", ", array));
+    }
+
+    public static void ContinueInstruction()
+    {
+        double[] factors = { 3, -2.2, 53, -8.4, 4.6, 12 };
+        double sum = 0;
+        for (int i = 0; i < factors.Length; i++)
+        {
+            if (factors[i] < 0)
+            {
+                continue;       //skacze do końca (138 linijka), pomija dalszą część iteracji
+            }
+            sum += factors[i];
+        }
+
+        Console.WriteLine(sum);
+    }
+
+    public static void BreakInstruction()
+    {
+        bool[] states = { true, true, true, false, true, false, true };
+        bool result = true;
+        //oblicz iloczyn logiczny z tablicy states
+        foreach (var state in states)
+        {
+            if (!state)
+            {
+                result = false;
+                break;          //skacze do końca iteracji(linijka 156)
+            }
+        }
+        //czyli tu
+        Console.WriteLine($"Iloczyn wynosi: {result}");
+    }
+
+
+    //Oblicz alternatywę dla tablicy states
+    public static void Task5()
+    {
+        bool[] states = { false, false, true, true, true, true, true };
+        bool result = true;
+        foreach (var state in states)
+        {
+            if (state)
+            {
+                result = false;
+                break;
+            }
+        }
+        Console.WriteLine($"Iloczyn wynosi: {result}");
+    }
 }
+
+
