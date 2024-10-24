69cbc254973daf171eea7e0b0f8785fbed6e3374
Bartlomiej_Wieloch <bartlomiej.wieloch@microsoft.wsei.edu.pl>
2024-10-24 22:22:41 +0200

    Completed Zadanie 2

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Zadanie1();
+            //Zadanie1();
+            Zadanie2();
         }
 
         static void Zadanie1()
@@ -16,6 +17,17 @@
             bool canPay = balans >= price && isCreditCardValid;
 
             Console.WriteLine(canPay);
+        }
+
+        static void Zadanie2()
+        {
+            Console.WriteLine("\nZadanie 2\n");
+            double a = 5;
+            double b = 1;
+            double c = 3;
+            bool isTriangle = (a + b) > c && (a + c) > b && (b + c) > a;
+            
+            Console.WriteLine(isTriangle);
         }
     }
 }
