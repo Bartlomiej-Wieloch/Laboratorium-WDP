2420a59a47d051dc68f2822b006cd1cc1a9ff7a5
Bartlomiej_Wieloch <bartlomiej.wieloch@microsoft.wsei.edu.pl>
2024-11-01 21:39:27 +0100

    Completed Zadanie 1

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Zadanie1();
+        }
+
+        public static void Zadanie1()
+        {
+            double[] factors = { 2.4, 1.9, -0.5 };
+            double x = 2.3;
+            
+            if (factors.Length == 3 )
+            {
+                Console.WriteLine(Math.Pow(x, 2)* factors[0] + x * factors[1] + factors[2]);
+            }
+            else if (factors.Length < 3)
+            {
+                Console.WriteLine("Nie można obliczyć równania, nie ma wszystkich współczynników.");
+            }
+            else
+            {
+                Console.WriteLine("Podano zbyt dużą ilość współczynnik");
+            }
         }
     }
 }
